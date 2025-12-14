7e5237a2d6433136ffa895fc9915914630374935
JFossey <justinfossey@gmail.com>
2025-12-14 13:58:55 +0200

    Setup vuejs,vite and tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,37 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
+        vue(),
     ],
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
+        },
+    },
+    // Adjust Vites dev server to work with DDEV
+    // https://vitejs.dev/config/server-options.html
+    server: {
+        // Respond to all network requests
+        host: "0.0.0.0",
+        port: 5173,
+        strictPort: true,
+        // Defines the origin of the generated asset URLs during development,
+        // this must be set to the Vite dev server URL and selected port.
+        origin: `${process.env.DDEV_PRIMARY_URL_WITHOUT_PORT}:5173`,
+        // Configure CORS securely for the Vite dev server to allow requests
+        // from *.ddev.site domains, supports additional hostnames (via regex).
+        // If you use another `project_tld`, adjust this value accordingly.
+        cors: {
+            origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
         },
     },
 });
